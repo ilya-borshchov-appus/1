bfed9c551d74a03cdaf437ce22a84152217a6783
atoshka <ilya.borshchov@gmail.com>
2014-08-20 11:29:34 +0300

    oneM

---
+++ JSSettingsViewController 2.m
@@ -0,0 +1,13 @@
+#import "JSSettingsViewController.h"
+
+@interface JSSettingsViewController ()
+
+@property (nonatomic, strong) NSArray *one1;
+
+@end
+
+
+@implementation JSSettingsViewController
+
+
+@end
